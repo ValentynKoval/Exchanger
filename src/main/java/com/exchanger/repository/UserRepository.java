a51bb85d53702dae44028831b9256c7f5441f45a
Valentyn_Koval <J.K.play2015@gmail.com>
2026-06-10 17:32:38 +0300

    create user repository

---
+++ src/main/java/com/exchanger/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.exchanger.repository;
+
+import com.exchanger.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    List<User> findAllByEmailOrPhone(String email, String phone);
+}
