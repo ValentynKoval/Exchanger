35316a1cc9a41c87a6fac99016996e28ef080dcd
Valentyn_Koval <J.K.play2015@gmail.com>
2026-06-10 14:48:36 +0300

    create user entity

---
+++ src/main/java/com/exchanger/entity/User.java
@@ -0,0 +1,22 @@
+package com.exchanger.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private Long telegramChatId;
+    private boolean active;
+}
