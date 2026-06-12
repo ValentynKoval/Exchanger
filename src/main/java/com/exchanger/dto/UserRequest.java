deebd9bf053d1435bba7c5ccf3a169c85410ebe9
Valentyn_Koval <J.K.play2015@gmail.com>
2026-06-12 15:11:41 +0300

    add annotation for validate mobile phone

--- src/main/java/com/exchanger/dto/UserRequest.java
+++ src/main/java/com/exchanger/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package com.exchanger.dto;
 
+import com.exchanger.validator.Phone;
 import jakarta.validation.constraints.Email;
 
-public record UserRequest (String firstName, String lastName, @Email String email, String phone) {}
+public record UserRequest (String firstName, String lastName, @Email String email, @Phone String phone) {}
